4b3b23c8cde3ca5d2b5d5560d10c805bb0a42a5a
Bekzat <jokenbe@mail.ru>
2024-04-22 17:01:39 +0500

    lab10

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h4>Асет чорт</h4>")
+    return render(request, 'main/index.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
